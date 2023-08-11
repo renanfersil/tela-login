ef04270cbbe845218ccafaee3b277ca1a84318c5
Renan Fernandes <101608000+renanfersil@users.noreply.github.com>
2023-08-11 18:31:46 -0300

    update

--- app.js
+++ app.js
@@ -7,3 +7,32 @@
       alert("Usuário ou Senha incorretos!");
     }
   }*/
+
+  // Obter elementos do formulário
+
+/*
+function validar() {
+var username = document.querySelector('input#username')
+var password = document.querySelector('input#password')
+
+
+  // Verificar se os campos estão preenchidos
+  if (username.value() === '' || password.value() === '') {
+    alert('Por favor, preencha todos os campos!')
+    return;
+  }
+
+  // Validar login e senha
+  if (username.value === 'usuario' && password.value === 'senha') {
+    alert('Login bem-sucedido! Redirecionando para a página principal...')
+    // Aqui você pode redirecionar o usuário para a página principal
+  } else {
+    alert('Credenciais inválidas! Tente novamente.')
+    // Aqui você pode limpar os campos de login e senha e se concentrar no campo de login novamente
+    username.value = ''
+    password.value = ''
+    username.focus()
+  }
+}
+
+*/
