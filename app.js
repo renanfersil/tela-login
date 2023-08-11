2912ec266b76e44d887f5281bbb35461416b6bbc
Renan Fernandes <101608000+renanfersil@users.noreply.github.com>
2023-08-11 16:19:39 -0300

    update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function validate() {
-    var username = document.querySelector("input#text").value;
-    var password = document.getElementById("input#password").value;
+/*function validate() {
+    var username = document.querySelector("input#text")
+    var password = document.getElementById("input#password")
     if (username === "renanpqd" && password === "renan1607") {
       alert("Login com sucesso!");
     } else {
       alert("Usuário ou Senha incorretos!");
     }
-  }
+  }*/
